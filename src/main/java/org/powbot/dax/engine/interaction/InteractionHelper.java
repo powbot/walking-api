9162e55118bd9b30b4cdc1fa6c1528181d1f1bb1
constt <ijavaservices@gmail.com>
2022-10-24 20:47:06 +1300

    start on interactions

--- src/main/java/org/powbot/dax/engine/interaction/InteractionHelper.java
+++ src/main/java/org/powbot/dax/engine/interaction/InteractionHelper.java
@@ -1,24 +1,20 @@
 package org.powbot.dax.engine.interaction;
 
-import org.tribot.api.General;
-import org.tribot.api.Timing;
-import org.tribot.api.interfaces.Clickable07;
-import org.tribot.api.interfaces.Positionable;
-import org.tribot.api2007.*;
-import org.tribot.api2007.types.*;
+import org.powbot.api.Locatable;
+import org.powbot.api.Tile;
+import org.powbot.api.rt4.*;
 import org.powbot.dax.shared.helpers.AccurateMouse;
-import org.powbot.dax.engine.WaitFor;
 
 import java.util.function.Predicate;
 
 
 public class InteractionHelper {
 
-    public static boolean click(Clickable07 clickable, String... actions){
+    public static boolean click(Interactive clickable, String... actions){
         return click(clickable, actions, null);
     }
 
-    public static boolean click(Clickable07 clickable, String action, WaitFor.Condition condition){
+    public static boolean click(Interactive clickable, String action, WaitFor.Condition condition){
         return click(clickable, new String[]{action}, condition);
     }
 
@@ -31,12 +27,12 @@ public class InteractionHelper {
      * @return if {@code condition} is null, then return the outcome of condition.
      *          Otherwise, return the result of the click action.
      */
-    public static boolean click(Clickable07 clickable, String[] actions, WaitFor.Condition condition){
+    public static boolean click(Interactive clickable, String[] actions, WaitFor.Condition condition){
         if (clickable == null){
             return false;
         }
 
-        if (clickable instanceof RSItem){
+        if (clickable instanceof Item){
             return clickable.click(actions) && (condition == null || WaitFor.condition(Random.nextInt(7000, 8000), condition) == WaitFor.Return.SUCCESS);
         }
 
@@ -65,8 +61,8 @@ public class InteractionHelper {
         }
 
         if (!AccurateMouse.click(clickable, actions)){
-            if (Camera.getCameraAngle() < 90){
-                Camera.setCameraAngle(Random.nextInt(90, 100));
+            if (Camera.pitch() < 90){
+                Camera.pitch(Random.nextInt(90, 100));
             }
             return false;
         }
@@ -74,50 +70,33 @@ public class InteractionHelper {
         return condition == null || WaitFor.condition(Random.nextInt(7000, 8500), condition) == WaitFor.Return.SUCCESS;
     }
 
-    public static RSItem getRSItem(Predicate<RSItem> filter){
-        RSItem[] rsItems = Inventory.find(filter);
-        return rsItems.length > 0 ? rsItems[0] : null;
+    public static Item getRSItem(Predicate<Item> filter){
+        return Inventory.stream().filter(filter).first();
     }
 
-    public static RSNPC getRSNPC(Predicate<RSNPC> filter){
-        RSNPC[] rsnpcs = NPCs.findNearest(filter);
-        return rsnpcs.length > 0 ? rsnpcs[0] : null;
+    public static Npc getRSNPC(Predicate<Npc> filter){
+        return Npcs.stream().filter(filter).nearest().first();
     }
 
-    public static RSObject getRSObject(Predicate<RSObject> filter){
-        RSObject[] objects = Objects.findNearest(15, filter);
-        return objects.length > 0 ? objects[0] : null;
+    public static GameObject getRSObject(Predicate<GameObject> filter){
+        return Objects.stream(15).filter(filter).nearest().first();
     }
 
-    public static RSGroundItem getRSGroundItem(Predicate<RSGroundItem> filter){
-        RSGroundItem[] groundItems = GroundItems.findNearest(filter);
-        return groundItems.length > 0 ? groundItems[0] : null;
+    public static GroundItem getRSGroundItem(Predicate<GroundItem> filter) {
+        return GroundItems.stream().filter(filter).nearest().first();
     }
 
-    public static boolean focusCamera(Clickable07 clickable){
+    public static boolean focusCamera(Interactive clickable){
         if (clickable == null){
             return false;
         }
-        if (isOnScreenAndClickable(clickable)){
+        if (clickable.inViewport()){
             return true;
         }
-        Tile tile = ((Positionable) clickable).getPosition();
-        Camera.turnToTile(tile);
-        Camera.setCameraAngle(100 - (tile.distanceTo(Players.local().tile()) * 4));
-        return isOnScreenAndClickable(clickable);
-    }
-
-    private static boolean isOnScreenAndClickable(Clickable07 clickable){
-        if (clickable instanceof RSCharacter && !((RSCharacter) clickable).isOnScreen()){
-            return false;
-        }
-        if (clickable instanceof RSObject && !((RSObject) clickable).isOnScreen()){
-            return false;
-        }
-        if (clickable instanceof RSGroundItem && !((RSGroundItem) clickable).isOnScreen()){
-            return false;
-        }
-        return clickable.isClickable();
+        Tile tile = ((Locatable) clickable).tile();
+        Camera.turnTo(tile);
+        Camera.pitch((int) (100 - (tile.distanceTo(Players.local().tile()) * 4)));
+        return clickable.inViewport();
     }
 
 
