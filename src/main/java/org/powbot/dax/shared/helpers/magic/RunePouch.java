329071efe1697f4c35f7b55376c15a0c60de711d
5Henveru2 <52henveru@outlook.com>
2023-02-27 17:06:56 -0600

    Add Divine rune pouch

--- src/main/java/org/powbot/dax/shared/helpers/magic/RunePouch.java
+++ src/main/java/org/powbot/dax/shared/helpers/magic/RunePouch.java
@@ -8,15 +8,26 @@ import java.util.Arrays;
 
 public class RunePouch{
 
-    private static int	SLOT_1_TYPE_BIT = 29, SLOT_1_QUANTITY_BIT = 1624, SLOT_2_TYPE_BIT = 1622, SLOT_2_QUANTITY_BIT = 1625, SLOT_3_TYPE_BIT = 1623, SLOT_3_QUANTITY_BIT = 1626;
+   private static final int
+			SLOT_1_TYPE_BIT = 29,
+			SLOT_1_QUANTITY_BIT = 1624,
+			SLOT_2_TYPE_BIT = 1622,
+			SLOT_2_QUANTITY_BIT = 1625,
+			SLOT_3_TYPE_BIT = 1623,
+			SLOT_3_QUANTITY_BIT = 1626,
+			SLOT_4_TYPE_BIT = 14285,
+			SLOT_4_QUANTITY_BIT = 14286;
+
+   private static final String[]
+            POUCH_NAMES = {"Rune pouch", "Divine rune pouch"};
 
     public enum RuneSlot {
         FIRST (SLOT_1_TYPE_BIT,SLOT_1_QUANTITY_BIT),
         SECOND (SLOT_2_TYPE_BIT,SLOT_2_QUANTITY_BIT),
-        THIRD (SLOT_3_TYPE_BIT,SLOT_3_QUANTITY_BIT);
+        THIRD (SLOT_3_TYPE_BIT,SLOT_3_QUANTITY_BIT),
+        FOURTH(SLOT_4_TYPE_BIT,SLOT_4_QUANTITY_BIT);
 
-        private int type;
-        private int quantityVarbitIndex;
+        private final int type, quantityVarbitIndex;
 
         RuneSlot(int type, int quantity){
             this.type = type;
@@ -72,9 +83,7 @@ public class RunePouch{
     }
 
     private static boolean hasPouch(){
-        return Inventory.stream().name("Rune pouch").isNotEmpty() && Worlds.isCurrentWorldMembers();
+        return Inventory.stream().name(POUCH_NAMES).isNotEmpty() && Worlds.isCurrentWorldMembers();
     }
-
-
 
 }
