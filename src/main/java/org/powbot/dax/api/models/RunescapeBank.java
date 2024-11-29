9554ac0ef3b5de0feb7dead49f2c41991480002d
5Henveru2 <96345909+5Henveru2@users.noreply.github.com>
2024-11-28 21:50:03 -0600

    Add Huey bank

--- src/main/java/org/powbot/dax/api/models/RunescapeBank.java
+++ src/main/java/org/powbot/dax/api/models/RunescapeBank.java
@@ -76,7 +76,7 @@ public enum RunescapeBank {
     YANILLE(2613, 3093, 0),
     ZANARIS(2383, 4458, 0),
     ZEAH_SAND_BANK(1719, 3465, 0),
-    HUEJCOATL_BANK(1527, 3292, 0),
+    HUEYCOATL_BANK(1527, 3292, 0),
     ;
     Tile position;
     RunescapeBank(int x, int y, int z){
